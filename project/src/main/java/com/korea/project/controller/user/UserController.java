15beeb02cb56c0a33a6fa6173c169ad9deb00a33
정상필의GRAM\mod45 <mod459@naver.com>
2024-07-01 20:17:34 +0900

    0701 정상필 - 회원가입

--- project/src/main/java/com/korea/project/controller/user/UserController.java
+++ project/src/main/java/com/korea/project/controller/user/UserController.java
@@ -1,8 +1,13 @@
 package com.korea.project.controller.user;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
+	@GetMapping("/register")
+	public String registerPage() {
+		return "all/register";
+	}
 }
