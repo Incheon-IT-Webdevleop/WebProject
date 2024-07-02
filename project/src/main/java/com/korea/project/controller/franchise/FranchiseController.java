fa51dcc1ad2981c0eeadb7a34c7020b6d85e2f39
LeeHwanHee <dlghksgml555@naver.com>
2024-07-02 17:39:30 +0900

    07/02 이환희 - 프렌차이즈 리스트 불러오기 구현

--- project/src/main/java/com/korea/project/controller/franchise/FranchiseController.java
+++ project/src/main/java/com/korea/project/controller/franchise/FranchiseController.java
@@ -2,8 +2,6 @@ package com.korea.project.controller.franchise;
 
 import com.korea.project.service.franchise.FranchiseService;
 import com.korea.project.vo.franchise.FranchiseVO;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -25,6 +23,6 @@ public class FranchiseController {
         List<FranchiseVO> franchises = franchiseService.getAllFranchises();
         model.addAttribute("franchises", franchises);
         System.out.println("Franchises added to model: " + franchises.size());
-        return "franchiseList";
+        return "/franchise/franchiseList";
     }
 }
--- project/src/main/java/com/korea/project/service/franchise/FranchiseService.java
+++ project/src/main/java/com/korea/project/service/franchise/FranchiseService.java
@@ -3,12 +3,16 @@ package com.korea.project.service.franchise;
 import com.korea.project.mapper.franchise.FranchiseMapper;
 import com.korea.project.vo.franchise.FranchiseVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class FranchiseService {
+
     private final FranchiseMapper franchiseMapper;
 
     @Autowired
@@ -17,8 +21,16 @@ public class FranchiseService {
     }
 
     public List<FranchiseVO> getAllFranchises() {
-        List<FranchiseVO> franchises = franchiseMapper.selectAllFranchises();
-        System.out.println("Retrieved franchises: " + franchises.size());
-        return franchises;
+        try {
+            List<FranchiseVO> franchises = franchiseMapper.selectAllFranchises();
+            System.out.println("Retrieved franchises: " + franchises.size());
+            return franchises;
+        } catch (DataAccessException e) {
+            // 예외 처리
+            System.err.println("Failed to retrieve franchises: " + e.getMessage());
+            throw e; // 예외 다시 던지기
+        }
     }
+
+    // 필요한 다른 비즈니스 로직 메서드 추가 가능
 }
