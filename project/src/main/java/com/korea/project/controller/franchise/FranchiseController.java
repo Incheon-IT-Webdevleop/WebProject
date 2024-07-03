cf71e547c9107a4e5a7578c8591a190c74a119c3
LeeHwanHee <dlghksgml555@naver.com>
2024-07-03 17:56:51 +0900

    0703 이환희 - 카테고리 버튼및 해당 카테고리 이동 구현 카페 디저트, 음식점 술집, 데이터 삽입후 출력 확인

--- project/src/main/java/com/korea/project/controller/franchise/FranchiseController.java
+++ project/src/main/java/com/korea/project/controller/franchise/FranchiseController.java
@@ -6,11 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Controller
 public class FranchiseController {
+
     private final FranchiseService franchiseService;
 
     @Autowired
@@ -22,7 +25,20 @@ public class FranchiseController {
     public String getAllFranchises(Model model) {
         List<FranchiseVO> franchises = franchiseService.getAllFranchises();
         model.addAttribute("franchises", franchises);
-        System.out.println("Franchises added to model: " + franchises.size());
+        return "/franchise/franchiseList";
+    }
+
+    @GetMapping("/franchisesBySector")
+    public String getFranchisesBySector(@RequestParam(name = "sector", required = false) Integer sector, Model model) {
+        List<FranchiseVO> franchises;
+
+        if (sector != null) {
+            franchises = franchiseService.getFranchisesBySector(sector);
+        } else {
+            franchises = franchiseService.getAllFranchises();
+        }
+
+        model.addAttribute("franchises", franchises);
         return "/franchise/franchiseList";
     }
 }
