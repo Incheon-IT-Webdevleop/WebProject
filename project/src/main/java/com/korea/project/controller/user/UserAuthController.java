904d53253fe0e006934e39450826f30ee1bf961a
정상필의GRAM\mod45 <mod459@naver.com>
2024-07-03 21:23:36 +0900

    0703 정상필 - 로그인페이지 진짜로 소셜미디어 로그인 뺴고 기능 구현 끝

--- project/src/main/java/com/korea/project/controller/user/UserAuthController.java
+++ project/src/main/java/com/korea/project/controller/user/UserAuthController.java
@@ -2,11 +2,18 @@ package com.korea.project.controller.user;
 
 import java.util.HashMap;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.korea.project.service.user.UserDetailServiceImpl;
 import com.korea.project.vo.user.UserVO;
@@ -19,6 +26,7 @@ public class UserAuthController {
 	
 	private final BCryptPasswordEncoder bCryptPasswordEncoder;
 	private final UserDetailServiceImpl userService;
+	private final AuthenticationManager authenticationManager;
 	
 	@GetMapping("/user/index")
 	public String index() {
@@ -26,11 +34,36 @@ public class UserAuthController {
 	}
 	
 	// 로그인 페이지에 점근하면 view 보여주기
-	@GetMapping("/loginPage")
-	public String loginPage() {
+    @GetMapping("/login")
+    public ModelAndView login() {
+        return new ModelAndView("user/login/login");
+    }
+    
+    @PostMapping("/perform_login")
+    public ModelAndView performLogin(@RequestParam("username") String username,
+                                     @RequestParam("password") String password) {
+        ModelAndView modelAndView = new ModelAndView();
 
-			return "user/login/login";
-	}
+        // 사용자 정의 유효성 검사
+        if (username.isEmpty() || password.isEmpty()) {
+            modelAndView.addObject("error", "아이디와 비밀번호를 입력해주세요.");
+            modelAndView.setViewName("login");
+            return modelAndView;
+        }
+
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(username, password)
+            );
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            modelAndView.setViewName("redirect:/user/index");
+        } catch (AuthenticationException e) {
+            modelAndView.addObject("error", "로그인에 실패했습니다.");
+            modelAndView.setViewName("login");
+        }
+
+        return modelAndView;
+    }
 	
 	// 회원가입 페이지에 접근하면 view보여주기
 	@GetMapping("/register")
@@ -52,29 +85,23 @@ public class UserAuthController {
 		return map;
 	}
 	
-	@PostMapping("/aip/check-duplicate")
+	@PostMapping("/api/check-duplicate")
 	@ResponseBody
-	public String checkDuplicate(String type, String value) {
-			if(type.equals("id")) {
-				
-			}else if(type.equals("email")) {
-				
-			}else if(type.equals("nickname")) {
-				
-			}else {
-				return "";
-			}
-		return "";
+	public HashMap<String, String> checkDuplicate( String type, String value) {
+		HashMap<String, String> map = new HashMap<>();	
+		if(type.equals("id")) {
+			
+			return map;
+		}else if(type.equals("email")) {
+			return map;
+		}else if(type.equals("nickname")) {
+			return map;
+		}else {
+			return map;
+		}
 	}
 
 	
-//	@PostMapping("/perform_login")
-//	@ResponseBody
-//	public HashMap<String, String> postLogin(LoginRequestDTO loginRequestDTO){
-//		HashMap<String, String> map = new HashMap<>();
-//		
-//		map.put("parma","success");
-//		return map;
-//	}
-	
-}
+
+}	
+
